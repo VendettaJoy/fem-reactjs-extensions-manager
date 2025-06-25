7a2f5f1d2ef7ca838012c46dc1aa6024cf449d60
Vendetta Joy <44125834+VendettaJoy@users.noreply.github.com>
2025-06-25 21:33:10 +0530

    Code Card
    Created a Card UI component to house each extension

--- src/components/List/ListItem.jsx
+++ src/components/List/ListItem.jsx
@@ -1,15 +1,22 @@
 import Button from "../UI/Button/Button";
+import Card from "../UI/Card/Card";
 import Toggle from "../UI/Toggle/Toggle";
 
 const ListItem = ({ item, onDelete, onToggle }) => {
 	return (
-		<li>
-			<img src={item.logo} alt="" />
-			<h2>{item.name}</h2>
-			<p>{item.description}</p>
-			<Button label="Remove" onClick={() => onDelete(item)} />
-            <Toggle name={`${item.name}-extension`} isActive={item.isActive} onClick={(e) => onToggle(e, item)}/>
-		</li>
+		<Card>
+			<li>
+				<img src={item.logo} alt="" />
+				<h2>{item.name}</h2>
+				<p>{item.description}</p>
+				<Button label="Remove" onClick={() => onDelete(item)} />
+				<Toggle
+					name={`${item.name}-extension`}
+					isActive={item.isActive}
+					onClick={(e) => onToggle(e, item)}
+				/>
+			</li>
+		</Card>
 	);
 };
 export default ListItem;
