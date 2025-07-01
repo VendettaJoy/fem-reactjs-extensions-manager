820592b27e53460fe67a7e446ceb7196858209fc
Vendetta Joy <44125834+VendettaJoy@users.noreply.github.com>
2025-07-01 14:38:02 +0530

    Make remove extension button SR friendly
    With only the text "Remove" visible on the button, it's hard to know which extension it refers to when using a screen reader. So using the sr-only CSS utility class additional information can be given to screen readers, but will not be visible on page

--- src/components/List/ListItem.jsx
+++ src/components/List/ListItem.jsx
@@ -3,13 +3,19 @@ import Card from "../UI/Card/Card";
 import Toggle from "../UI/Toggle/Toggle";
 
 const ListItem = ({ item, onDelete, onToggle }) => {
+	const label = (
+		<>
+			Remove <span className="sr-only">{item.name} Extension</span>
+		</>
+	);
+
 	return (
 		<Card>
 			<li>
 				<img src={item.logo} alt="" />
 				<h2>{item.name}</h2>
 				<p>{item.description}</p>
-				<Button label="Remove" onClick={() => onDelete(item)} />
+				<Button label={label} onClick={() => onDelete(item)} />
 				<Toggle
 					isActive={item.isActive}
 					onClick={(e) => onToggle(e, item)}
