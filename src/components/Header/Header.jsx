04dca5021a666b3d5962d9608c382008972c8e6e
Vendetta Joy <44125834+VendettaJoy@users.noreply.github.com>
2025-07-03 14:13:16 +0530

    Style Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,23 +3,25 @@ import { ThemeCtx } from "../../context/ThemeContext";
 import Logo from "./Logo";
 import Moon from "../../assets/images/icon-moon.svg";
 import Sun from "../../assets/images/icon-sun.svg";
+import classes from "./header.module.css"
 
 const Header = () => {
 	const { theme, toggleTheme } = useContext(ThemeCtx);
 	const isDark = theme === "light" ? false : true;
 
 	return (
-		<header>
+		<header className={classes.header}>
 			<Logo />
 			<button
 				type="button"
 				onClick={toggleTheme}
+                className={classes.themeToggle}
 				aria-label="Toggle Dark Mode"
 				aria-pressed={isDark}
 			>
 				<img
 					src={theme === "light" ? Moon : Sun}
-					alt="Theme Icon"
+					alt="Theme Toggle Icon"
 					aria-hidden="true"
 				/>
 			</button>
