757affaf6623ef55deb29f85192db3003145f58c
Vendetta Joy <44125834+VendettaJoy@users.noreply.github.com>
2025-07-04 14:48:02 +0530

    Style List Item

--- src/components/List/ListItem.jsx
+++ src/components/List/ListItem.jsx
@@ -1,24 +1,29 @@
 import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
 import Toggle from "../UI/Toggle/Toggle";
+import classes from "./listItem.module.css"
 
 const ListItem = ({ item, onDelete, onToggle }) => {
 	return (
-		<li>
+		<li className={classes.listItem}>
 			<Card>
-				<img src={item.logo} alt="" />
-				<h2>{item.name}</h2>
-				<p>{item.description}</p>
-				<Button
-					label="Remove"
-					onClick={() => onDelete(item)}
-					ariaLabel={`Remove ${item.name} Extension`}
-				/>
-				<Toggle
-					isActive={item.isActive}
-					onClick={(e) => onToggle(e, item)}
-					ariaLabel={`Toggle ${item.name} Extension`}
-				/>
+				<div className={classes.info}>
+                    <img src={item.logo} alt="" />
+                    <h2>{item.name}</h2>
+                    <p>{item.description}</p>
+                </div>
+				<div className={classes.buttons}>
+                    <Button
+                        label="Remove"
+                        onClick={() => onDelete(item)}
+                        ariaLabel={`Remove ${item.name} Extension`}
+                    />
+                    <Toggle
+                        isActive={item.isActive}
+                        onClick={(e) => onToggle(e, item)}
+                        ariaLabel={`Toggle ${item.name} Extension`}
+                    />
+                </div>
 			</Card>
 		</li>
 	);
